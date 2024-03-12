2bfd15948b647b4618d518c8affe2f22a4b4deaa
sarah-kamall <sarahkamal1712@gmail.com>
2024-03-12 14:19:44 +0200

    test save done

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,6 +5,7 @@ Test suits for the base model
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
+import time
 
 class TestBaseModel(unittest.TestCase):
     """
@@ -44,6 +45,13 @@ class TestBaseModel(unittest.TestCase):
         my_model.my_number = 89
         mystr = "[{}] ({}) {}".format('BaseModel', my_model.id, my_model.__dict__)
         self.assertEqual(mystr, my_model.__str__())
+    
+    def test_save(self):
+        my_model = BaseModel()
+        my_time = my_model.updated_at
+        time.sleep(0.001)
+        my_model.save()
+        self.assertNotEqual(my_time,my_model.updated_at)
 
 if __name__ == "__main__":
     unittest.main()
